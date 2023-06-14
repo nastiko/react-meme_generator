b16b176d54cb50bc8d985ca0edfaf9fedecc118e
Anastasia <anastasia.hrynkevich@outlook.com>
2023-06-14 17:12:51 +0100

    started to learn how to use 'state'

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Header from "./components/Header"
 import Meme from "./components/Meme"
 
 export default function App() {
+
     return (
         <div>
             <Header />
-            <Meme />
+            <Meme
+            />
 
         </div>
     )
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,25 @@
-import React from "react"
+ import React from "react"
+import defaultData from "../data";
 
 export default function Meme() {
+
+    const [memeItems, getMemeItems] = React.useState("");
+
+    function getMemeImg() {
+        const memesArray = defaultData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+
+        getMemeItems(prevState => memesArray[randomNumber].url);
+
+    }
+
     return (
         <section>
             <form action="#" className="form">
                 <input className="form--input" type="text" placeholder="Some text"/>
                 <input className="form--input" type="text" placeholder="Another text"/>
-                <button className="form--button">Get a new meme image</button>
+                <button onClick={getMemeImg} className="form--button">Get a new meme image</button>
+                <img className="img--style" src={memeItems} alt="Meme" />
             </form>
         </section>
     )
